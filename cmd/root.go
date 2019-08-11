8116efd3c62ed68e3cabddceb0cc0c1631f532c5
Ran Tavory <rantav@gmail.com>
2019-08-11 14:53:05 +0300

    Remove extra newline

--- cmd/root.go
+++ cmd/root.go
@@ -11,8 +11,7 @@ var rootCmd = &cobra.Command{
 	Use:   "go-archetype",
 	Short: "A templating tool",
 	Long:  `Use go-archetype to transform project archetypes into existing live projects`,
-	Run: func(cmd *cobra.Command, args []string) {
-	},
+	Run:   func(cmd *cobra.Command, args []string) {},
 }
 
 // Execute the current command
