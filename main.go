cf9dc27b7c5ccf6ef6ee78c949f42edb73575acc
Viacheslav Isaev <viacheslav.isaev@bitstamp.net>
2023-10-04 17:36:20 +0200

    Adding select-promter.

--- main.go
+++ main.go
@@ -8,6 +8,18 @@ import (
 // See the README.md file for specification
 // END __INCLUDE_README__
 
+// BEGIN __SIMPLE__
+// Simple section
+// END __SIMPLE__
+
+// BEGIN __MEDIUM__
+// Medium section
+// END __MEDIUM__
+
+// BEGIN __ADVANCED__
+// Advanced section
+// END __ADVANCED__
+
 func main() {
 	cmd.Execute()
 }
